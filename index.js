c3432f750214d97c8b9704478f8d94f351995530
nabin <thakurathinabin@gmail.com>
2020-12-15 21:34:30 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require('morgan');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const router = express.Router();
