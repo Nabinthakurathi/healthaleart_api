45a6d40fcf664135014f9f948957b1814e666a67
nabin <thakurathinabin@gmail.com>
2020-12-15 21:40:29 +0545

    first commit

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const morgan = require('morgan');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const router = express.Router();
+
+
+const app = express();
+app.use(morgan('tiny'));
+app.use(express.json());
+app.options('*',cors());
+app.use(e)
